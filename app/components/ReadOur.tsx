9b194c09b3fea705e9b3faf3bea6816fabda7888
Muhammadali-fr <boburjonr934@gmail.com>
2025-04-29 11:32:37 +0500

    home page just started

---
+++ app/components/ReadOur.tsx
@@ -0,0 +1,34 @@
+
+"use client"
+import { useState } from "react";
+
+// router 
+import Link from "next/link";
+
+// lucide icons 
+import { X } from "lucide-react";
+
+
+export default function ReadOur() {
+    const [isHidden, setIsHidden] = useState(true);
+
+    // close btn clikced 
+    const handleClose = () => {
+        setIsHidden(!isHidden);
+    }
+
+    return (
+
+        <div >
+            {
+                isHidden && <header className="text-center  bg-[#1B1B2D] flex items-center justify-between px-2">
+                    <div></div>
+                    <Link className="underline" href={"/terms"}>read our philosophy creating this platform</Link>
+                    {/* close icom  */}
+                    <X onClick={handleClose} className="cursor-pointer" />
+                </header>
+            }
+        </div>
+
+    )
+}
