3a4aecbaff4b7cd1d212c1fffd27c31d3ac341e6
Muhammadali-fr <boburjonr934@gmail.com>
2025-05-04 19:02:16 +0500

    switch and select ui

--- app/(pages)/create-post/page.tsx
+++ app/(pages)/create-post/page.tsx
@@ -1,6 +1,17 @@
 
-//  antd 
-import { Select } from "antd";
+// shadcn select ui 
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/app/reuseable/ui/select";
+
+// switch toogle 
+import { Switch } from "@/app/reuseable/ui/switch";
+
+
 
 export default function CreatePost() {
     return (
@@ -30,12 +41,27 @@ export default function CreatePost() {
                     <input type="file" />
                 </label>
 
-                <Select className="w-full bg-[#1B1B2D]"
-                    options={[
-                        { value: 'jack', label: 'Jack' },
-                        { value: 'lucy', label: 'Lucy' },
-                    ]}
-                />
+                {/* select ui  */}
+                <label>
+                    <p>collection (optional)</p>
+                    <Select >
+                        <SelectTrigger className="w-full bg-[#1B1B2D] py-5 border border-none">
+                            <SelectValue placeholder="Not in collection" />
+                        </SelectTrigger>
+                        <SelectContent className="bg-[#1B1B2D] text-white border-none">
+                            <SelectItem value="light">Light</SelectItem>
+                            <SelectItem value="dark">Dark</SelectItem>
+                            <SelectItem value="system">System</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </label>
+
+                {/* toggle  */}
+                <label>
+                    <p>Allow all comments Comments</p>
+                    <Switch className="bg-black" />
+                </label>
+
             </form>
         </div>
     )
