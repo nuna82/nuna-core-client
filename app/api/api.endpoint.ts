d94b018f6fae5fda741ae1076edaee9b73386e1f
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-05-04 13:41:43 +0500

    i pushed this cuz i've forgot create new branch

--- app/api/api.endpoint.ts
+++ app/api/api.endpoint.ts
@@ -1,6 +1,46 @@
 export const apiendpoin = {
     // Home
-    home: "home",
-    login: "login",
-    getUsers: "getUsers"
+    home: "",
+
+    // Users and User details
+    getUsers: "users/all",
+
+    // register user
+    registerUser: "auth/register",
+
+    // verify user
+    verifyMagicLink: "auth/verify-magic-link",
+
+    // login user
+    loginUser: "auth/login",
+
+    // user profile
+    userProfile: "auth/profile",
+
+
+    // only one user
+    getOneUser: "users/:id",
+
+    // delete user
+    deleteUser: "users/delete/:id",
+
+    // Change User Deatils
+    changeUserDetails: "users/update/:id",
+
+    // collections
+
+    // posts and post details
+    createNewPost: "posts/new",
+
+    // get all posts
+    getAllPosts: "posts/many",
+
+    // get only one post from  id
+    getOnePost: "posts/:id",
+
+    // update post
+    updatePost: "posts/update/:id",
+
+    // delete post
+    deletePost: "posts/remove/:id",
 };
